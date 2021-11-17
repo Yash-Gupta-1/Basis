95a5085764a21d16821945fcced349415e8fb9f9
Yash Gupta <yashgupta162001@gmail.com>
2021-11-18 02:34:16 +0530

    design home, login and signup page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>Basis Test Task</h1>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        {/* Login  */}
+        <Route path="/" element={<Login />} />
+        {/* Signup */}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
